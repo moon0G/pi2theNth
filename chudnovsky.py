707a2b0cc21fdac1ffa35a49b1b5c57ad21d781c
Byron Fanti <bfant@mybce.catholic.edu.au>
2024-01-26 23:21:53 +1000

    commit varients

---
+++ chudnovsky.py
@@ -0,0 +1,27 @@
+import decimal
+
+# source: https://en.wikipedia.org/wiki/Chudnovsky_algorithm
+# this is best case while using recursion, recursion used for calcuating to the Nth decimal.
+def binary_split(a, b):
+    if b == a + 1:
+        Pab = -(6*a - 5)*(2*a - 1)*(6*a - 1)
+        Qab = 10939058860032000 * a**3
+        Rab = Pab * (545140134*a + 13591409)
+    else:
+        m = (a + b) // 2
+        Pam, Qam, Ram = binary_split(a, m)
+        Pmb, Qmb, Rmb = binary_split(m, b)
+        
+        Pab = Pam * Pmb
+        Qab = Qam * Qmb
+        Rab = Qmb * Ram + Pam * Rmb
+    return Pab, Qab, Rab
+
+
+def chudnovsky(n):
+    P1n, Q1n, R1n = binary_split(1, n)
+    return (426880 * decimal.Decimal(10005).sqrt() * Q1n) / (13591409*Q1n + R1n)
+
+
+for n in range(2,2000):
+    print(f"{n=} {chudnovsky(n)}")  # 3.14159265358979323846264338...
